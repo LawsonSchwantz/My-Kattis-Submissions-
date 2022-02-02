2418989c0039a03e1b1d35519cd0dee1010c3cab
Bertrand R. M <74954683+LawsonSchwantz@users.noreply.github.com>
2022-02-02 16:25:51 +0700

    Create parking.cpp

---
+++ parking.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+
+
+int main(){
+
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int t;
+        cin >> t;
+
+        int s, l, temp;
+        cin >> temp;
+        s = temp;
+        l = temp;
+
+        for(int i = 0; i < t - 1; i++) {
+            cin >> temp;
+            s = min(s, temp);
+            l = max(l, temp);
+        }
+
+        cout << (l - s) * 2 << endl;
+    }    
+}
