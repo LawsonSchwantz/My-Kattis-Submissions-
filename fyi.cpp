870f5a340f0a5037929f277db1f8f3102b4781e2
Bertrand R. M <74954683+LawsonSchwantz@users.noreply.github.com>
2022-07-03 18:58:09 +0700

    Create fyi.cpp

---
+++ fyi.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+int main(){
+
+    char t[7];
+    scanf("%s", &t);
+    if(t[0] == '5' && t[1] == '5' & t[2] == '5'){
+        puts("1");
+    }else{
+        puts("0");
+    }
+
+
+    return 0;
+
+}
